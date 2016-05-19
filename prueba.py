96b9b9eecbac723aac7eb2364c36f6ee528f9db2
rubenpozo <ruben_oso28@hotmail.com>
2016-05-19 07:21:46 -0500

    plantilla
    plantilla para implementar los ejercicios

---
+++ prueba.py
@@ -0,0 +1,4 @@
+print('ESCUELA POLITECNICA NACIONAL'+'\nPrueba Primer Bimestre\n')
+print('Escoja una de las siguientes opciones:'+'\n1.-suma de vectores'+'\n2.-Ingresar numeros'+'\n3.-dividir en tres partes un archivo de texto'+'\n4.-Salir')
+
+
