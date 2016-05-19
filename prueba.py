fec2837469eb02148a50e30d9f3f00808f0e5893
rubenpozo <ruben_oso28@hotmail.com>
2016-05-19 07:46:55 -0500

    primera parte
    primera parte

--- prueba.py
+++ prueba.py
@@ -1,4 +1,12 @@
 print('ESCUELA POLITECNICA NACIONAL'+'\nPrueba Primer Bimestre\n')
-print('Escoja una de las siguientes opciones:'+'\n1.-suma de vectores'+'\n2.-Ingresar numeros'+'\n3.-dividir en tres partes un archivo de texto'+'\n4.-Salir')
-
-
+opc=int(input('Escoja una de las siguientes opciones:'+'\n1.-suma de vectores'+'\n2.-Ingresar numeros'+'\n3.-dividir en tres partes un archivo de texto'+'\n4.-Salir\n'))
+if opc==1:
+	b=[]
+	c=[]
+	for i in range(0,3):
+		x=int(input('ingrese posicion:'+str(i)+' del primer vector'))
+		b.append(x)
+	for j in range(0,3):
+		y=int(input('ingrese posicion:'+str(j)+' del segundo vector'))
+		c.append(y)
+pass
