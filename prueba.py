13fe4fe7ce032f1dbbe2ae9622b1d6f673b06521
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-05-19 07:50:38 -0500

    editado
    se puede cambiar

--- prueba.py
+++ prueba.py
@@ -1,12 +1,16 @@
 print('ESCUELA POLITECNICA NACIONAL'+'\nPrueba Primer Bimestre\n')
-opc=int(input('Escoja una de las siguientes opciones:'+'\n1.-suma de vectores'+'\n2.-Ingresar numeros'+'\n3.-dividir en tres partes un archivo de texto'+'\n4.-Salir\n'))
+opc=int(input('Escoja una de las siguientes opciones:'+'\n1.- Suma de vectores'+'\n2.- Ingresar numeros'+'\n3.- Dividir en tres partes un archivo de texto'+'\n4.- Salir\n'))
 if opc==1:
 	b=[]
 	c=[]
+	d=[]
 	for i in range(0,3):
-		x=int(input('ingrese posicion:'+str(i)+' del primer vector'))
+		x=int(input('Ingrese posicion '+str(i)+' del primer vector: '))
 		b.append(x)
 	for j in range(0,3):
-		y=int(input('ingrese posicion:'+str(j)+' del segundo vector'))
+		y=int(input('ingrese posicion '+str(j)+' del segundo vector: '))
 		c.append(y)
+	for k in range(0,3):
+		z=int(input('ingrese posicion '+str(k)+' del tercer vector: '))
+		d.append(y)
 pass
